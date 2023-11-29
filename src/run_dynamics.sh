29318fe21c8d985aa412fadf161e764f5c65a859
jej127 <skdlcm456@korea.ac.kr>
2023-11-29 17:20:44 +0900

    debug

--- src/run_dynamics.sh
+++ src/run_dynamics.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-n=1
-seed=111
+n=3
 
 #For MNLI
 train_data='MNLI'
-seed=111
 bert_model='google/bert_uncased_L-2_H-128_A-2'
 learning_rate=3e-5
 weight_decay=0.1
@@ -13,4 +11,5 @@ warmup_steps=2000
 logging_steps=2000
 adam_epsilon_b=1e-8
 
+seed=222
 CUDA_VISIBLE_DEVICES=$n python train_bert_dynamics.py --output_dir ../experiments/bert_weak_nli --bert_model $bert_model --train_data $train_data --do_train --do_eval --mode vn --seed $seed --learning_rate $learning_rate --weight_decay $weight_decay --num_train_epochs $num_train_epochs --warmup_steps $warmup_steps --logging_steps $logging_steps --adam_epsilon 1e-8
--- src/run_last_layer_biased.sh
+++ src/run_last_layer_biased.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-n=1
+n=3
 
 #For MNLI
 learning_rate=3e-5
@@ -10,7 +10,7 @@ weight_decay=0.1
 bert_model='google/bert_uncased_L-2_H-128_A-2'
 num_train_epochs=3.0
 
-seed=111
+seed=222
 q=0.20
 CUDA_VISIBLE_DEVICES=$n python train_bert_poe_td.py --output_dir ../experiments/bert_weak_nli --training_dynamics_path $training_dynamics_path --train_data $train_data --bert_model $bert_model --do_train --do_eval --do_eval_on_train --mode bin_td --seed $seed --learning_rate $learning_rate --weight_decay $weight_decay --num_train_epochs $num_train_epochs --warmup_steps $warmup_steps --adam_epsilon 1e-8 --q $q
 
